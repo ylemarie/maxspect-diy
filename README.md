ec92cd2b7ca753a999024a2f88624648069cbde7
Yann <yann.lemarie@gmail.com>
2016-10-17 18:12:08 +0200

    [UPD] initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# maxspect-diy
-NodeJS program to control Maxspect Razor like Led Light
+Projet 
+------
+Pouvoir gérer mes 8 rampes Maxspect Razor DIY http://www.recifal-france.fr/rampe-diy-maxpect-r420-150-t26852.html
+
+Contraintes : 
+- interface web pour visualiser la T° des rampes, le % de pwm en cours
+- interface web pour contrôler le % de pwm, le switch on/off des ventilateurs
+- définir comme sur les Maxspect des "Time Period" avec %blue et %white sur des tranches horaires
+- mettre en route les ventilos sur une T° de consigne
+- avoir une 2ième sécurité sur la T°, ie une atténuation de xx% de chaque canal si le ventilo n'arrive pas à refroidir la rampe entre 2 cycles de contrôle de la T°
+
+Materiels
+---------
+* RaspberryPi3 : http://fr.farnell.com/raspberry-pi/raspberrypi3-modb-1gb/sbc-raspberry-pi-3-mod-b-1gb-ram/dp/2525225?MER=BN-PDP-2525225
+* Carte ServoPi : https://www.abelectronics.co.uk/p/44/Servo-PWM-Pi
+* Carte 8 relais : http://www.banggood.com/5V-8-Channel-Relay-Module-Board-For-Arduino-PIC-AVR-DSP-ARM-p-74110.html
+* Sonde de T° : http://www.banggood.com/DS18B20-Waterproof-Digital-Temperature-Temp-Sensor-Probe-1M-2M-3M-5M-10M-15M-p-983801.html
+* Ventilos : http://www.banggood.com/12V-Internal-Desktop-Computer-CPU-Case-Cooling-Cooler-Master-Silent-Fan-7cm-p-997479.html
+* Alims réglables : http://www.banggood.com/5Pcs-4V-40V-DC-DC-Step-Down-LM2596-Voltage-Regulator-Converter-Module-p-946548.html
+
+Branchements 
+------------
+* sondes de T° sur le GPIO4 : 
+	http://www.framboise314.fr/mesure-de-temperature-1-wire-ds18b20-avec-le-raspberry-pi/
+	http://www.framboise314.fr/wp-content/uploads/2014/02/schema_connexion_600px.jpg
+
+* carte ServoPi :
+	https://www.abelectronics.co.uk/p/44/Servo-PWM-Pi
+	Chaque Dimmer SureElectronics est connecté sur le pin [-] et le pin [pwm] de la carte sur les Pin 1 à 8
+	La carte est alimentée en 5V externe avec le [-] relié aux [-] des Dimmer
+
+* carte 8 Relais :
+	Chaque Pwm est relié à un GPIO du Raspberry3 : GPIO11=relais1, GPIO12=relais2, ..., GPIO18=relais8
+	La carte relais est alimentée en 5V externe (la même)
+
+* carte IO Pi Plus :
+	Je suis tombé sur un bug de la lib ABElectronics, ils l'ont corrigé hier: https://www.abelectronics.co.uk/forums/thread/io-pi-plus-and-servo-pwm-pi-zero-with-nodejs/#lastpost
+	TODO: refaire tests avec la carte
+
+* RaspberryPi3 :
+	Le raspberry est alimentée en 5V externe (la même) par l'intermédiare de la carte ServoPi
+
+Installation
+------------
+sudo apt-get install nodejs
+mkdir -p ~/projet/maxspect/public/
+
+recopier l'arborescence :
+public/
+	atten-off.png
+	atten-on.png
+	fan-off.png
+	fan-on.png
+	index.html
+	off.jpg
+	on.jpg
+	slider.css
+	yle.css
+
+Installation librairie ABElectronics
+------------------------------------
+cd ~/projet/maxspect
+git clone https://github.com/abelectronicsuk/ABElectronics_NodeJS_Libraries.git
+
+Installation modules NodeJS
+---------------------------
+cd ~/projet/maxspect
+
+npm install express
+npm install http
+npm install socket.io
+
+npm install ds18x20
+
+npm install rpio
+npm install onoff
+
+npm install moment
+npm install twix
+npm install dateformat
+
+Execution
+---------
+cd ~/projet/maxspect
+sudo node app.js
+
+appli web : http://<ip rapsberry>:8989
---
+++ app.js
@@ -0,0 +1,323 @@
+//Begin const ---------------------------------------------
+
+var NB_RAMPES = 8;			//nb rampe to manage
+var SERVER_PORT = 8989;		//http port
+var TEMP_VENTILO = 20;		//T° to set ON ventillo
+var TEMP_ATTENUATION = 25;	//T° to set attenuation ON
+var ATTENUATION_SCALE = 10;		//attenuation factor = -10%
+var CHECK_PERIOD = 10;		//check period in seconds
+var ON = 0;					//relay card inverse ?
+var OFF = 1;				//relay card inverse ?
+var DEBUG = 1;				//debug mode
+var TPS = [					// Time Periods definition
+	{ hour: '08:00', blue:   0, white:   0 },
+	{ hour: '10:00', blue:  10, white:  20 },
+	{ hour: '11:00', blue:  80, white:  70 },
+	{ hour: '15:00', blue: 100, white:  90 },
+	{ hour: '17:00', blue:  80, white:  90 },
+	{ hour: '20:00', blue:  60, white:  50 },
+	{ hour: '24:00', blue:   0, white:  10 }
+];
+
+//global
+var attenuationLoop = [];
+for (var i=1; i <= NB_RAMPES; i++) { attenuationLoop[i]=0; }
+
+//End const -----------------------------------------------
+
+//Begin Required libraries --------------------------------
+
+//Server & socket
+var express = require('express');
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
+server.listen(SERVER_PORT);				//start server
+app.use(express.static('public'));		//static page
+
+//Gpio
+var Gpio = require('onoff').Gpio; 				// Constructor function for Gpio objects. (need npm install rpio)
+var gpioRelay = [];
+for (i=1; i<=NB_RAMPES; i++) {
+	if (DEBUG) { console.log("init gpioRelay "+i); }
+	gpioRelay[i-1] = new Gpio(i+10, 'out');     // Export GPIO #11-18 as an output for Relay Fan
+	gpioRelay[i-1].writeSync(OFF);				//set off
+}
+
+//Raspberry ServoPi
+var servopi = require('./ABElectronics_NodeJS_Libraries/lib/servopi/servopi');
+var pwm = new ServoPi(0x40);	// create an servopi object
+pwm.setPWMFrequency(1000);		// Set PWM frequency to 1 Khz and enable the output
+pwm.outputEnable();
+
+//TimePeriod
+var moment = require('moment');						//http://momentjs.com/docs
+var twix = require('twix');							//http://isaaccambron.com/twix.js/
+var dateFormat = require('dateformat');				//https://github.com/felixge/node-dateformat
+
+//Temperature
+var sensor = require('ds18x20');					//https://www.npmjs.com/package/ds18x20
+var rampeSensors = sensor.list();
+if (DEBUG) { console.log('T° Sensors Adr', rampeSensors); }
+var allRampeT = sensor.get(rampeSensors);			//sync
+if (DEBUG) { console.log('All T°', allRampeT); }
+
+//End Required libraries ----------------------------------
+
+//Begin managing hardware ---------------------------------
+
+//manage Temperature Sensors
+var getRampeT = function(sensorAdr) {
+	try {
+		temp = sensor.get(sensorAdr);
+	} catch (err) {	//if no sensor
+		temp = 0;
+	}
+	if (DEBUG) {  console.log("T° of "+sensorAdr+"="+temp); }
+	return temp;
+}
+
+//manage relay
+function setRelay(num, state) {
+	if (DEBUG) { console.log("setRelay n°"+num+" => "+state); }
+	actual = gpioRelay[num-1].readSync();
+	if ( actual != state ) {
+		if (DEBUG) { console.log("relay "+num+"("+actual+") changing to "+state); }
+		gpioRelay[num-1].writeSync(state);
+	} else {
+		if (DEBUG) { console.log("relay "+num+"("+actual+") already "+state); }
+	}
+}
+
+//manage pwm Led
+function setPwmLed(pwm, num, brightness) {
+	pwm_bright = Math.round(4095 - brightness * 4095 / 100);	//inverse for LDD SureElectronic
+    pwm.setPWM(num-1, 0, pwm_bright);							//pin n°num (pwm 0-15 = pin 1-16)
+    if (DEBUG) { console.log(num+'-auto) brightness='+brightness+'% pwm='+pwm_bright); }
+}
+
+//manage pwm Rampe
+function setPwmRampe (pwm, num, ratio) {	// = manage 2 LED
+	//rampe n°1 = pwm 1 & pwm 2		//rampe n°2 = pwm 3 & pwm 4		//...		//rampe n°8 = pwm 15 & pwm 16
+	setPwmLed(pwm, 2*num-1, ratio.blue);
+	setPwmLed(pwm, 2*num,   ratio.white);		
+}
+//End managing hardware -----------------------------------
+
+//Begin managing web request ------------------------------
+
+//manage relay switch
+function manageWebRelaySwitch(socket, num) {
+	var newValue = OFF;
+	var relayName = 'relayswitch'+num;
+	socket.on(relayName, function(data){
+		if (DEBUG) { console.log(num+"-web0 relayswitch !!!!!!!!!! =>"+data.state); }
+		if (data.state==OFF) {	
+			newValue = ON;
+		}
+		setRelay(num, newValue);
+		if (DEBUG) { console.log(num+"-web1 relayswitch !!!!!!!!!! =>"+newValue); }
+		io.sockets.emit(relayName, {state: newValue});
+	});
+	socket.emit(relayName, {state: newValue});	//init a 0
+}
+
+//manage % with slider & socket
+function manageWebLed(socket, pwm, num) {
+	var brightness = 0;
+	var ledName = 'led'+num; 	// led n°pwm
+	socket.on(ledName, function(data) {
+		brightness = data.value;		
+        //brigthness de 0 a 100
+        pwm_bright = 4095 - brightness * 4095 / 100;	//inverse pour LDD SureElectronic
+        pwm.setPWM(num-1, 0, pwm_bright);				//pin n°num (pwm 0-15 = pin 1-16)
+        if (DEBUG) { console.log(num+'-web) brightness='+brightness+'% pwm='+pwm_bright); }
+		io.sockets.emit(ledName, {value: brightness});	
+	});	
+	if (DEBUG) { console.log(num+'-web-init) brightness='+brightness+'% pwm='+pwm_bright); }
+	socket.emit(ledName, {value: brightness});	
+}
+function manageWebRampe(socket, pwm, num) {
+	//rampe n°1 = pwm 1 & pwm 2		//rampe n°2 = pwm 3 & pwm 4	//...	//rampe n°8 = pwm 15 & pwm 16
+	manageWebLed(socket, pwm, 2*num-1);
+	manageWebLed(socket, pwm, 2*num);
+}
+
+//manage % with auto-ratio & socket
+function manageWebLedRatio(socket, num, brightness) {
+	var ledName = 'led'+num;
+	socket.emit(ledName, {value: brightness});
+}
+function manageWebRampeRatio(socket, num, ratio, rampeInfos) {
+	manageWebLedRatio(socket, 2*num-1, ratio.blue);
+	manageWebLedRatio(socket, 2*num,   ratio.white);
+
+	var rampeName = 'rampe' + num;
+	io.sockets.emit( rampeName, {temp: rampeInfos.temp,relay: rampeInfos.relay, attenuation: rampeInfos.attenuation, infos:rampeInfos.infos} );
+}
+//End managing web request --------------------------------
+
+//Begin main program--- -----------------------------------
+
+//get ratio pwm for 1 specific hour
+function getPwm(hour) {	
+	if (DEBUG) { console.log("Looking for TP (" + hour + ") -----------------"); }
+	var rampe_pwm = { blue: -1, white: -1};
+	if ( !(TPS[0].hour < hour && hour < TPS[TPS.length-1].hour) ) {	//hour is in implicit period (night)
+		tp1 = TPS[TPS.length-1];
+		tp2 = TPS[0];
+		if (DEBUG) { console.log( 'night: ' + hour + " (" + TPS[TPS.length-1].hour + "-" + TPS[0].hour +")" ); }
+		if (DEBUG) { console.log('TP1-TP2', tp1, tp2 ); }
+		var ratio = ratioPwm(tp1, tp2, hour);
+	} else {	//find hour in TPS
+		for( i = 0; i < TPS.length-1; i++ ) {
+			tp1 = TPS[i];
+			tp2 = TPS[i+1];
+			if ( tp1.hour <= hour  && hour < tp2.hour) {
+				if (DEBUG) { console.log( tp1.hour + " <= " + hour + " < " + tp2.hour); }
+				if (DEBUG) { console.log( 'TP1-TP2', tp1, tp2 ); }
+				var ratio = ratioPwm(tp1, tp2, hour);
+			}
+		}
+	}
+	return ratio;
+}
+
+//get TimePeriod of 1 specific hour
+function getTP(hour) {	
+	if (DEBUG) { console.log("Looking for TP (" + hour + ") -----------------"); }
+	var tp = {}
+	if ( !(TPS[0].hour < hour && hour < TPS[TPS.length-1].hour) ) {	//hour is in implicit period (night)
+		tp1 = TPS[TPS.length-1];
+		tp2 = TPS[0];
+		if (DEBUG) { console.log( 'night: ' + hour + " (" + TPS[TPS.length-1].hour + "-" + TPS[0].hour +")" ); }
+		if (DEBUG) { console.log('TP1-TP2', tp1, tp2 ); }
+		tp = {tp1, tp2};
+	} else {	//find hour in TPS
+		for( i = 0; i < TPS.length-1; i++ ) {
+			tp1 = TPS[i];
+			tp2 = TPS[i+1];
+			if ( tp1.hour <= hour  && hour < tp2.hour) {
+				if (DEBUG) { console.log( tp1.hour + " <= " + hour + " < " + tp2.hour); }
+				if (DEBUG) { console.log( 'TP1-TP2', tp1, tp2 ); }
+				tp = {tp1, tp2};
+			}
+		}
+	}
+	return tp;
+}
+
+//ratio calcul
+function ratioPwm(tp1, tp2, hour) {
+	var duree_tp = moment("2000-01-01T" + tp1.hour).twix("2000-01-01T" + tp2.hour).count('minutes')-1;	//nb minutes of time period
+	if ( duree_tp < 0 ) {	//momment lost in calcul in case 23:00-08:00
+		var duree_tp_before00h = moment("2000-01-01T" + tp1.hour).twix("2000-01-01T24:00").count('minutes')-1;
+		var duree_tp_after00h = moment("2000-01-01T00:00").twix("2000-01-01T"  + tp2.hour).count('minutes')-1;
+		if (DEBUG) { console.log("night detected !" + duree_tp_before00h + "+" + duree_tp_after00h); }
+		duree_tp = duree_tp_before00h + duree_tp_after00h;
+		
+		if ( tp1.hour <= hour && hour <= "24:00" ) {
+			var duree_hour_before00h = moment("2000-01-01T" + hour).twix("2000-01-01T24:00").count('minutes')-1;		//nb minutes since time period begin
+			var duree_hour_after00h = 0;			
+		} else {
+			var duree_hour_before00h = moment("2000-01-01T" + tp1.hour).twix("2000-01-01T24:00").count('minutes')-1;
+			var duree_hour_after00h = moment("2000-01-01T00:00").twix("2000-01-01T" + hour).count('minutes')-1;			//nb minutes since time period begin
+		}
+		var duree_hour = duree_hour_before00h + duree_hour_after00h;
+	} else {
+		var duree_hour = moment("2000-01-01T" + tp1.hour).twix("2000-01-01T" + hour).count('minutes')-1;				//nb minutes since time period begin
+	}
+
+	//ratio 1 min = % increase/decrease by minute
+	ratio_1min = {
+			blue: (tp2.blue-tp1.blue)/duree_tp * 100,
+			white: (tp2.white-tp1.white)/duree_tp * 100,
+	}	
+	if (DEBUG) { console.log( "TP:" + duree_tp ); console.log( "Hour:" + duree_hour ); console.log( ratio_1min ); }
+
+	//ratio in %
+	ratio = { 
+		blue:  Math.round( (ratio_1min.blue * duree_hour)/100 + tp1.blue ),
+		white: Math.round( (ratio_1min.white * duree_hour)/100 + tp1.white ) 
+	}
+	return ratio;
+}
+
+//Manage All Ramp
+function manageAutoAllRampes() {
+	var now = new Date();
+	now_hour = dateFormat(now, "HH:MM");
+
+	var ratio = getPwm( now_hour );		//check Time Period Ratio
+	var tp = getTP( now_hour );			//check Time Period
+	if (DEBUG) { console.log('Ratio wanted',ratio); }
+
+	for (var i=1; i <= NB_RAMPES; i++) {		//8 rampes
+		if (DEBUG) { console.log("Rampe n°"+i+"..............."); }
+		var relay = -1;
+		var attenuation = 0;
+		var attenuationFactor = 0;		
+
+		var tempRampe = getRampeT( rampeSensors[i-1] );
+		//switch ventilo on/off ?
+		if ( tempRampe >= TEMP_VENTILO) {
+			relay = ON;
+		} else {
+			relay = OFF;
+			attenuationLoop[i]=0;
+		}
+		if (DEBUG) { console.log("T° rampe n°"+i+": T° rampe:"+tempRampe+" (max:"+TEMP_VENTILO+") Ventilo: "+relay); }
+		setRelay(i, relay);
+
+		//attenuation needed ?
+		if ( tempRampe >= TEMP_ATTENUATION) {
+			attenuationLoop[i]++;
+			attenuation = ATTENUATION_SCALE * attenuationLoop[i];			
+			attenuationFactor = 1 - attenuation / 100;
+			if ( attenuationFactor < 0 ) {
+				attenuationFactor = 0;		//OFF the rampe
+				attenuation = 100;
+			} 
+			new_ratio = { blue: Math.round(ratio.blue * attenuationFactor), white: Math.round(ratio.white * attenuationFactor) }; 	//attenuation -10% x nbre fois check
+		} else {
+			new_ratio = ratio;
+		}
+		if (DEBUG) { console.log("T° rampe n°"+i+": T° rampe:"+tempRampe+" (max:"+TEMP_ATTENUATION+") Attenuation: "+attenuation+"%"); }
+		if (DEBUG) { console.log('Ratio new',new_ratio); }
+		//set pwm & socket web
+		setPwmRampe(pwm, i, new_ratio);
+
+		var infos = now_hour;
+		infos += " ("+tp.tp1.hour+"-"+tp.tp2.hour+") ";
+		infos += " [B:"+tp.tp1.blue +"%-"+tp.tp2.blue+"% W:"+tp.tp1.white +"%-"+tp.tp2.white+"%]";
+		//infos += " [ blue:"+new_ratio.blue+"%, white:"+new_ratio.white+"% ]";
+		manageWebRampeRatio(io.sockets, i, new_ratio, {temp: tempRampe, relay: relay, attenuation: attenuation, infos: infos});	//io.sockets ? (seems ok)
+	}
+
+	return ratio;
+}
+
+//init
+var ratio = manageAutoAllRampes();
+if (DEBUG) { console.log('Init Ratio', ratio); }
+
+//every minute
+setInterval(function() {
+	ratio = manageAutoAllRampes();
+	if (DEBUG) { console.log('Ratio', ratio); }
+}, CHECK_PERIOD * 1000); // 60 * 1000 milsec
+if (DEBUG) { console.log("Running every "+CHECK_PERIOD+" seconds"); }
+
+//on socket ready
+io.sockets.on('connection', function (socket) {
+	if (DEBUG) { console.log('Socket connection'); }
+	//manageWebLed(socket, pwm, 1);
+	//manageWebLed(socket, pwm, 2);
+	for (var i=1; i <= NB_RAMPES; i++) {		//8 rampes
+		manageWebRampe(socket, pwm, i);			//manual
+		manageWebRampeRatio(socket, i, ratio, {temp:0, relay:0, attenuation:0, infos:"waiting for data ..."});	//auto
+		manageWebRelaySwitch(socket, i);
+	}
+});
+if (DEBUG) { console.log("Running web on port 8989"); }
+//End main program--- -------------------------------------
